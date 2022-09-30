6633fb69995994aed0ff7dbf374ed45b62f6a32d
Sagar Kashyap <sagarkashyap.cc@gmail.com>
2022-10-01 00:50:24 +0530

    app for measuring height of tall objects

---
+++ script.js
@@ -0,0 +1,30 @@
+function main(){
+    window.addEventListener("deviceorientation",onOrientationChange)
+    navigator.mediaDevices.getUserMedia({video:{
+        facingMode:'environment'
+    }})
+    .then(function(signal){
+        const video=document.getElementById("myVideo");
+        video.srcObject=signal;
+        video.play();
+    })
+    .catch(function(err){
+        alert(err);
+        console.log(err)
+    })
+}
+
+function onOrientationChange(event){
+
+    let angle=event.beta-90;
+    if(angle<0){
+        angle=0;
+    }
+    console.log(angle)
+    const distFromTree=document.getElementById("mySlider").value;
+    document.getElementById("myLabel").innerHTML=
+    "Distance from tree: "+distFromTree+" meters";
+    const height=Math.tan(angle*Math.PI/180)*distFromTree;
+    document.getElementById("heightInfo").innerHTML=
+    height.toFixed(1)+"m("+angle.toFixed(1)+"&deg;)";
+}
